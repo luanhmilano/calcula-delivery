17bfd36d5ef7a1a55951c4fa13d9a16e007a7cd6
Luan Henrique <luanhsantos14@gmail.com>
2025-06-25 00:46:49 -0300

    corrigindo erro de build

--- src/pages/Simulador/SimuladorPrecoPage.tsx
+++ src/pages/Simulador/SimuladorPrecoPage.tsx
@@ -241,7 +241,7 @@ export const SimuladorPrecoPage = ({ navigateTo, pratoInicial, onSave, isTestMod
                                 <ResponsiveContainer width="100%" height="100%">
                                     <PieChart>
                                         <Pie data={calculos.composicaoCustoData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={60} fill="#8884d8">
-                                            {calculos.composicaoCustoData.map((entry, index) => (
+                                            {calculos.composicaoCustoData.map((_entry, index) => (
                                                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                                             ))}
                                         </Pie>
